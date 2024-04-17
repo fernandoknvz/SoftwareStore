e6e11fbfcba24016af1900a41ffb1606561161a3
fernandoknvz <fe.olguea@gmail.com>
2024-04-17 15:08:10 -0400

    cambios_pagina_web

---
+++ validacion_producto.js
@@ -0,0 +1,17 @@
+function validacionProducto(){
+    //declaro variables a utilizar
+    let valor_talla;
+    let cantidad;
+    let total;
+
+    //obtengo la informacion
+    valor_talla = document.getElementById('valor_talla').value;
+    cantidad = document.getElementById('cantidad').value;
+    valor_talla = parseInt(valor_talla);
+    cantidad = parseInt(cantidad);
+
+    total = valor_talla * cantidad;
+
+    console.log('El total a pagar es: $',total);
+
+}
